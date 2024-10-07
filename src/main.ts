b2a850333104afa4b52588a2ee51d1bdadc8f142
Rifan <tubagusrifan@gmail.com>
2024-10-07 20:07:35 +0700

    first init

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { LoggingMiddleware } from './middleware/logging.middleware';
+
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // Middleware untuk logging
+  app.use(new LoggingMiddleware().use);
+
+  // Mengaktifkan CORS
+  app.enableCors();
+
+  // Mendengarkan pada port 3001 di localhost
+  await app.listen(3002, '127.0.0.1');
+}
+
+bootstrap();
